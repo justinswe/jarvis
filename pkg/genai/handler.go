95a340589709cc9a2e2ff534c746606b8b75c387
justinswe <fernbaughj@gmail.com>
2026-07-09 20:10:19 -0700

    v0.3.1

--- pkg/genai/handler.go
+++ pkg/genai/handler.go
@@ -16,7 +16,7 @@ const (
 	DefaultMaxOutputTokens   = 512
 	MaxOutputTokensLimit     = 512
 	selectedModel            = "google/gemini-3.5-flash"
-	verificationCaveat       = "I couldn't verify this with usable web sources."
+	verificationCaveat       = "I couldn't verify this with the web."
 	maxToolRounds            = 2
 	maxFunctionCallsPerRound = 2
 )
